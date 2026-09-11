7fd4542c0c8160f653277e3bf0569c7b0d72a483
Jack Sullin <jsullin4519@gmail.com>
2026-09-10 23:20:40 -0400

    Updated Type Filter :

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -43,7 +43,11 @@ function App() {
           <option>All</option>
           <option>Electric</option>
           <option>Gas</option>
-          <option>Electric</option>
+          <option>Water</option>
+          <option>Internet</option>
+          <option>Sewer</option>
+          <option>Car Insurance</option>
+          <option>Mortgage</option>
         </select>
         <main>
           <table>
